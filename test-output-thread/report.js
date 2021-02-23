ad2ffb7bf337018b151396b851d87b958c45b0d1
Madhu228 <bwsmadhukar@gmail.com>
2021-02-23 12:42:15 +0530

    1st push

---
+++ test-output-thread/report.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "a692ec99-b3b2-431a-9b52-f31c97c22814",
+    "feature": "To test the Shoppage functionality of Subsource",
+    "scenario": "Check Shop deatails",
+    "start": 1613728601090,
+    "group": 15,
+    "content": "",
+    "tags": "",
+    "end": 1613728625350,
+    "className": "passed"
+  },
+  {
+    "id": "36e4d272-f18d-4815-ac4d-ca05e5793b77",
+    "feature": "To test the Company functionality of Subsource",
+    "scenario": "Check Company deatails",
+    "start": 1613728601089,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1613728619509,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 14,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 15,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  }
+]);
+});
